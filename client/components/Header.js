2d1dfc0589659925d7697758778fc15c15cf7706
Mohit Aggarwal <mohitkumar06612@gmail.com>
2023-09-18 16:23:37 +1000

    initial commit

---
+++ client/components/Header.js
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <div>I am header</div>;
+};
+
+export { Header };
---
+++ client/pages/studentsLogin.js
@@ -0,0 +1,12 @@
+import { Header } from "../components/Header";
+
+const StudentsLogin = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Hi buddy</h1>
+    </div>
+  );
+};
+
+export default StudentsLogin;
