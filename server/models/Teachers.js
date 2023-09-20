203f3670b7b0ed5317ef97768380949e6ee7a90f
Mohit Aggarwal <mohitkumar06612@gmail.com>
2023-09-21 03:39:45 +1000

    students and teachers model created

---
+++ server/models/Teachers.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  const Teachers = sequelize.define("Teachers", {
+    // here all the structure for table
+
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true, // Ensure email addresses are unique
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    fullName: {
+      type: DataTypes.STRING, // Assuming full name is a string
+      allowNull: false, // You can set this to false if full name is required
+    },
+    phoneNumber: {
+      type: DataTypes.STRING, // Assuming phone number is a string
+      allowNull: true, // You can set this to false if phone number is required
+    },
+  });
+  return Teachers;
+};
