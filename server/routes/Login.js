8d3137e8b4dfd7afe23b0eaa5d781bf7744849cf
Mohit Aggarwal <46507817+Mohitkumar33@users.noreply.github.com>
2023-09-24 14:00:12 +1000

    student login and teacher login added (#3)

--- server/routes/Login.js
+++ server/routes/Login.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const { Admins } = require("../models");
+const { Students } = require("../models");
+const { Teachers } = require("../models");
 
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
@@ -28,6 +30,48 @@ router.post("/adminLogin", async (req, res) => {
   );
 
   res.json({ token, fullName: admin.fullName });
+  //   res.send("hello mohit");
+});
+
+router.post("/studentLogin", async (req, res) => {
+  const { email, password } = req.body;
+
+  // Find the student by email
+  const student = await Students.findOne({ where: { email } });
+  if (!student || !bcrypt.compareSync(password, student.password)) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  const token = jwt.sign(
+    { id: student.id, email: student.email },
+    process.env.REACT_APP_JWT_SECRET,
+    {
+      expiresIn: "1h", // Token expires in 1 hour
+    }
+  );
+
+  res.json({ token, fullName: student.fullName });
+  //   res.send("hello mohit");
+});
+
+router.post("/teacherLogin", async (req, res) => {
+  const { email, password } = req.body;
+
+  // Find the teacher by email
+  const teacher = await Teachers.findOne({ where: { email } });
+  if (!teacher || !bcrypt.compareSync(password, teacher.password)) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  const token = jwt.sign(
+    { id: teacher.id, email: teacher.email },
+    process.env.REACT_APP_JWT_SECRET,
+    {
+      expiresIn: "1h", // Token expires in 1 hour
+    }
+  );
+
+  res.json({ token, fullName: teacher.fullName });
   //   res.send("hello mohit");
 });
 
