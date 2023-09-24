6569ca05649626bf598b56b0dc01ef3b859c41e2
Mohit Aggarwal <46507817+Mohitkumar33@users.noreply.github.com>
2023-09-24 17:23:17 +1000

    Mohit frontend chakra setup (#5)
    * auth added
    
    * created the home page

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,17 @@
 import "@/styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const customTheme = extendTheme({
+    colors: {
+      customTeal: "#ED8936", // Define your custom color
+    },
+  });
+  return (
+    <>
+      <ChakraProvider theme={customTheme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
 }
