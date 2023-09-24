e254b4c2ca923451d54fb9a2e03d547548e1f274
Mohit Aggarwal <mohitkumar06612@gmail.com>
2023-09-24 17:22:45 +1000

    created the home page

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,6 +2,12 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { Header } from "@/components/Header";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import CallToActionWithIllustration from "@/components/HomePage";
+import Navigation from "@/components/homePage/Navigation";
+// import LargeWithAppLinksAndSocial from "@/components/homePage/Footer";
+import LargeWithLogoLeft from "@/components/homePage/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,8 +20,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      {/* <main className={`${styles.main} ${inter.className}`}>
         <h1>Hello</h1>
+      </main> */}
+      <main>
+        <Navigation />
+        <CallToActionWithIllustration />
+        <LargeWithLogoLeft />
       </main>
     </>
   );
