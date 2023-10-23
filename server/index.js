2f571480f715f4d70e9634ff170a78d1a32d1fa9
Mohit Aggarwal <46507817+Mohitkumar33@users.noreply.github.com>
2023-10-23 16:24:46 +1100

    attendence record function added (#25)

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3005;
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
 const db = require("./models");
@@ -14,12 +16,14 @@ const studentsRouter = require("./routes/Students");
 const teachersRouter = require("./routes/Teachers");
 const subjectsRouter = require("./routes/Subjects");
 const questionsRouter = require("./routes/Questions");
+const attendenceRouter = require("./routes/Attendence");
 
 app.use("/auth", loginRouter);
 app.use("/api/students", studentsRouter);
 app.use("/api/teachers", teachersRouter);
 app.use("/api/subjects", subjectsRouter);
 app.use("/api/questions", questionsRouter);
+app.use("/api/attendence", attendenceRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, Express!");
