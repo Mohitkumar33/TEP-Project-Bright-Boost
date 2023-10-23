73e33ccba349275b1eeddcb3388a177fe6dfe9f6
neerajraj22 <148020418+neerajraj22@users.noreply.github.com>
2023-10-23 14:16:42 +1100

    Update _app.js (#16)
    made some changes in the root app folder. Like adding title. And importing

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,9 @@
-import "@/styles/globals.css";
+import { AuthProvider } from "@/components/context/auth-context";
+import LargeWithLogoLeft from "@/components/homePage/Footer";
+import Navigation from "@/components/homePage/Navigation";
+// import "@/styles/globals.css";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   const customTheme = extendTheme({
@@ -9,9 +13,16 @@ export default function App({ Component, pageProps }) {
   });
   return (
     <>
-      <ChakraProvider theme={customTheme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
+      <AuthProvider>
+        <ChakraProvider theme={customTheme}>
+          <Head>
+            <title>Bright Boost</title>
+          </Head>
+          <Navigation />
+          <Component {...pageProps} />
+          <LargeWithLogoLeft />
+        </ChakraProvider>
+      </AuthProvider>
     </>
   );
 }
