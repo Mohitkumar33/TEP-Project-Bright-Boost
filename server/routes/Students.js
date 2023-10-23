f24f295fff230b5dde1e87b9e1c16e6da84bae9c
neerajraj22 <148020418+neerajraj22@users.noreply.github.com>
2023-10-23 16:52:11 +1100

    added auth required in student login (#30)
    I added authenticateToken middleware in student login

--- server/routes/Students.js
+++ server/routes/Students.js
@@ -1,15 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { Students } = require('../models');
+const { Students } = require("../models");
+const authenticateToken = require("../middleware/auth");
 
 // Route to get all students
-router.get('/', async (req, res) => {
+router.get("/", authenticateToken, async (req, res) => {
   try {
     const students = await Students.findAll();
     res.json(students);
   } catch (error) {
-    console.error('Error fetching students:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error fetching students:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
